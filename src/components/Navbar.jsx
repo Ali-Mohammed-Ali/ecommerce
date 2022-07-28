8699722339281890756fd483c1e9786d174cd5c7
unknown <alimohammed8798@gmail.com>
2022-07-28 07:14:46 +0200

    finishing the slider and categories

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,8 @@
+import { Badge } from '@material-ui/core'
+import { Search, ShoppingCartOutlined } from '@material-ui/icons'
 import React from 'react'
 import styled from 'styled-components'
+//    Start  Style 
 const Container = styled.div`
     height: 60px;
 `
@@ -7,23 +10,72 @@ const Wrapper = styled.div `
     padding: 10px 20px;
     display: flex;
     justify-content: space-between;
+    align-items: center;
 `
-const Left = styled.div`
-    width: 33.3%
+//Start left side 
+const Left = styled.div `
+    flex: 1;
+    display: flex;
+    align-items: center;
 `
-const Center = styled.div`
-    width: 33.3%
+const Lang = styled.span `
+    font-size: 14px;
+    cursor: pointer;
 `
-const Right = styled.div`
-    width: 33.3%
+const SearchContainer = styled.div `
+    border: 0.5px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
 `
+const Input = styled.input `
+    border: none;
+`
+// Start center side
+const Center = styled.div `
+    flex: 1;
+    text-align: center;
+`
+const Logo = styled.h1 `
+    font-weight: bold;
+`
+// Start right side
+const Right = styled.div `
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+`
+const MenuItem = styled.div `
+    font-size: 14px;
+    cursor: pointer;
+    margin-left: 25px;
+`
+// End Style
 const Navbar = () => {
     return (
         <Container>
             <Wrapper>
-                <Left>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam deserunt sunt, perspiciatis obcaecati maxime iste, nihil quasi id ex illo dolorum. Ut quas obcaecati ipsam fugit, nesciunt accusantium aliquid consectetur!</Left>
-                <Center>center</Center>
-                <Right>right</Right>
+                <Left>
+                    <Lang>EN</Lang>
+                    <SearchContainer>
+                        <Input />
+                        <Search style= {{color: 'gray', fontSize: '16px'}}/>
+                    </SearchContainer>
+                </Left>
+                <Center>
+                    <Logo>LUMA.</Logo>
+                </Center>
+                <Right>
+                    <MenuItem>REGISTER</MenuItem>
+                    <MenuItem>SIGN IN</MenuItem>
+                    <MenuItem>
+                        <Badge badgeContent={4} color="primary">
+                            <ShoppingCartOutlined />
+                        </Badge>
+                    </MenuItem>
+                </Right>
             </Wrapper>
         </Container>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Announcement from '../components/Announcement';
 import Navbar from '../components/Navbar';
 
 
 const Home = () => {
     return (
         <div>
+            <Announcement />
             <Navbar />
         </div>
     )
